f6e83f25defa786d0105fa52aae708dce09c4a52
Johannes Schriewer <dark@coulomb.localdomain>
2020-12-14 15:03:01 +0100

    Bugfix: Typo in filename

--- package/planticam_stillcapture/planticam_stillcapture.mk
+++ package/planticam_stillcapture/planticam_stillcapture.mk
@@ -23,7 +23,7 @@ endef
 define PLANTICAM_STILLCAPTURE_INSTALL_INIT_SYSTEMD
 	mkdir -p $(TARGET_DIR)/etc/systemd/system/$(PLANTICAM_STILLCAPTURE_INIT_SYSTEMD_TARGET)
 	mkdir -p $(TARGET_DIR)/usr/lib/systemd/system
-	$(INSTALL) -D -m 644 $(PLANTICAM_STILLCAPTURE_PKGDIR)planticam_still.service $(TARGET_DIR)/usr/lib/systemd/system/planticam_sill.service
+	$(INSTALL) -D -m 644 $(PLANTICAM_STILLCAPTURE_PKGDIR)planticam_still.service $(TARGET_DIR)/usr/lib/systemd/system/planticam_still.service
 	ln -sf /usr/lib/systemd/system/planticam_still.service $(TARGET_DIR)/etc/systemd/system/$(PLANTICAM_STILLCAPTURE_INIT_SYSTEMD_TARGET)
 endef
 
